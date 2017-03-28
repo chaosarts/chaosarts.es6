5c8f812b7556d77a5e13cdeea01b90ca7819a07f
Fu Lam Diep <fulam.diep@chaosarts.de>
2017-03-28 03:20:57 +0200

    FIX enableClass

--- ui.js
+++ ui.js
@@ -8,14 +8,27 @@ export { Component } from './ui/Component';
  * @param {String...} classname 
  */
 export function enableClass (element, enable, classname) {
-    let classes = element.getAttribute('class').replace(/\s+/g, ' ').split();
-    let classNamesToAdd = Array.prototype.slice.call(arguments, 2); 
+    let elementClasses = element.getAttribute('class').replace(/\s+/g, ' ').split(' ');
+    let inClasses = Array.prototype.slice.call(arguments, 2); 
+    let outClasses = new Array();
     
-    while (classNamesToAdd.length > 0) {
-        let cls = classNamesToAdd.shift();
-        if (classes.indexOf(cls) < 0)
-            classes.push(cls);
+    if (enable) {
+        outClasses = elementClasses;
+
+        while (inClasses.length > 0) {
+            let inClass = inClasses.shift();
+            if (elementClasses.indexOf(inClass) < 0)
+                elementClasses.push(inClass);
+        }
+
+    }
+    else {
+        while (elementClasses.length > 0) {
+            const elementClass = elementClasses.shift();
+            if (inClasses.indexOf(elementClass) < 0)
+                outClasses.push(elementClass);
+        }   
     }
 
-    element.setAttribute('class', classes.join(" "));
+    element.setAttribute('class', outClasses.join(" "));
 }
