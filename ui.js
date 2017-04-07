a212a1d376112d7517f91322473c8f35094ddd97
Fu Lam Diep <fulam.diep@chaosarts.de>
2017-04-07 14:11:09 +0200

    UPDATE Umbrella include files

--- ui.js
+++ ui.js
@@ -1,2 +1,5 @@
 export * from './ui/fn';
 export { Component } from './ui/Component';
+export { AppComponent } from './ui/AppComponent';
+export { Canvas2DComponent } from './ui/Canvas2DComponent';
+export { WebGLComponent } from './ui/WebGLComponent';
