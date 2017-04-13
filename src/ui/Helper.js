887c91f8f72bfd1f45c4d1c649f451f0c06d3ab2
Fu Lam Diep <fulam.diep@chaosarts.de>
2017-04-13 04:13:50 +0200

    UPDATE Helper class. Renaming setup and cease to init and deinit

--- src/ui/Helper.js
+++ src/ui/Helper.js
@@ -1,5 +1,4 @@
 let key2class = new Map();
-let key2instance = new Map();
 
 class Helper {
 
@@ -48,33 +47,135 @@ class Helper {
             return null;
         }
 
-        if (!key2instance.has(key)) {
-            const ctor = key2class.get(key);
-            key2instance.get(new ctor());
-        }
-
-        return key2instance.get(name);
+        const ctor = key2class.get(key);
+        return new ctor();
     }
 
 
     /**
-     * Setup the component
+     * Accessor to get the name with which the helper has been associated
+     * @public
+     * @return {string}
+     */
+    get name () {
+        return this._name;
+    }
+
+
+    /**
+     * Accessor to get the component to help
+     * @public
+     * @return {comopnent}
+     */
+    get component () {
+        return this._component;
+    }
+
+
+    /**
+     * Accessor to indicate whether the helper has been initialized or not
+     * @public
+     * @return {boolean} 
+     */
+    get initialized () {
+        return this._initPromise != null;
+    }
+
+
+    get burntOut () {
+        return this._component == null;
+    }
+
+
+    /**
+     * Creates a new instance of helper class
+     * @param {string} name
+     * @param {Component} component
+     */
+    constructor (name, component) {
+
+        /**
+         * Provides the name with which this helper has been associated
+         * @private
+         * @type {string}
+         */
+        this._name = name;
+
+        /**
+         * The component to help
+         * @private
+         * @type {Component}
+         */
+        this._component = component;
+
+        /**
+         * Indicates whether the helper has been initialized or not
+         * @private
+         * @type {Promise}
+         */
+        this._initPromise = null;
+
+
+        this._deinitPromise = null;
+    }
+
+
+    /**
+     * Initializes the helper
      * @public
      * @param {Component} component
      * @param {string} helperName
      */
-    setup (component, helperName) {
+    init () {
+
+        if (!this._initPromise) {
+            if (this.burntOut) {
+                this._initPromise = Promise.reject(`Helper ${this._name} is burnt out.`);
+            }
+            else {
+                try {
+                    const result = this._init();
+                    this._initPromise = result instanceof Promise ? /** @type {Promise} */ (result) : Promise.resolve(result);
+                }
+                catch (error) {
+                    this._initPromise = Promise.reject(error);
+                }
+            }
+        }
+
+        return this._initPromise;
+    }
+
+
+    /**
+     * Reverts the init function
+     * @public
+     * @param {Component} component
+     * @param {string} helperName
+     */
+    deinit () {
+        if (!this._deinitPromise) {
+            try {
+                const result = _deinit();
+                this._deinitPromise = result instanceof Promise ? /** @type {Promise} */ (result) : Promise.resolve(result);
+            }
+            catch (error) {
+                this._deinitPromise = Promise.resolve(`Could not deinitialize helper ${this._name}: ${error.message}`);
+            }
+
+            this._component = null;
+            this._initPromise = null;
+        }
+
+    }
+
+
+    _init () {
         throw new Error('Subclass of Helper for Component must implement setup()');
     }
 
 
-    /**
-     * Reverts the setup
-     * @public
-     * @param {Component} component
-     * @param {string} helperName
-     */
-    cease (component, helperName) {
+    _deinit () {
         throw new Error('Subclass of Helper for Component must implement cease()');
     }
 }
